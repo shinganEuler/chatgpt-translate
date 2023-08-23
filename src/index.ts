3c546bc77d243eda057b0ec8d0d0d637358aeb26
shinganEuler <88284684+shinganEuler@users.noreply.github.com>
2023-08-23 16:38:59 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,59 +27,64 @@ interface OpenAIResponseUsage {
     total_tokens: number;
 }
 
-export async function translateWithOpenAI(
-    OPENAI_API_KEY: string,
-    message: string,
-    openai_url: string,
-    model: string,
-    target_langualge: string
-): Promise<string> {
-    const data = {
+async function requestAPI(apiUrl: string, data: any, headers: Record<string, string>): Promise<OpenAIResponse> {
+    const parsedUrl = url.parse(apiUrl, true);
+    const hostname = parsedUrl.hostname;
+    const pathname = (parsedUrl.pathname || "") + (parsedUrl.search || "");
+
+    const options: https.RequestOptions = {
+        hostname: hostname,
+        path: pathname,
+        method: 'POST',
+        headers: headers
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res: any) => {
+            let body = '';
+            res.setEncoding('utf8');
+            res.on('data', (chunk: any) => {
+                body += chunk;
+            });
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(body) as OpenAIResponse;
+                    resolve(response);
+                } catch (error: any) {
+                    reject(new Error(`Failed to parse API response: ${error.message}`));
+                }
+            });
+        });
+
+        req.on('error', (error: any) => {
+            reject(new Error(`Request to API failed: ${error.message}`));
+        });
+
+        req.write(JSON.stringify(data));
+        req.end();
+    });
+}
+
+function constructData(message: string, targetLanguage: string, model?: string): any {
+    return {
         model: model,
         messages: [
-            { role: 'system', content: "translate all my input to " + target_langualge },
+            { role: 'system', content: `Please provide a straightforward translation of the following contents into ${targetLanguage}. Do not add any extra information or context.` },
             { role: 'user', content: message }
         ],
         temperature: 0,
     };
+}
 
+export async function translateWithOpenAI(OPENAI_API_KEY: string, message: string, openai_url: string, model: string, targetLanguage: string): Promise<string> {
     try {
-        const parsedUrl = url.parse(openai_url, true);
-        const hostname = parsedUrl.hostname;
-        const pathname = (parsedUrl.pathname || "") + (parsedUrl.search || "");
-
-        const options: https.RequestOptions = {
-            hostname: hostname,
-            path: pathname,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${OPENAI_API_KEY}`
-            }
+        const data = constructData(message, targetLanguage, model);
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_API_KEY}`
         };
 
-        const response: OpenAIResponse = await new Promise((resolve, reject) => {
-            const req = https.request(options, (res: any) => {
-                let body = '';
-                res.setEncoding('utf8');
-                res.on('data', (chunk: any) => {
-                    body += chunk;
-                });
-                res.on('end', () => {
-                    try {
-                        const response = JSON.parse(body) as OpenAIResponse;
-                        resolve(response);
-                    } catch (error: any) {
-                        reject(new Error(`Failed to parse openai response: ${error.message}`));
-                    }
-                });
-            });
-            req.on('error', (error: any) => {
-                reject(new Error(`Request openai failed: ${error.message}`));
-            });
-            req.write(JSON.stringify(data));
-            req.end();
-        });
+        const response = await requestAPI(openai_url, data, headers);
 
         if (response.choices.length === 0) {
             throw new Error('Openai no response choices found');
@@ -97,57 +102,15 @@ export async function translateWithOpenAI(
     }
 }
 
-export async function translateWithAzureOpenAI(
-    AZURE_KEY: string,
-    message: string,
-    azure_openai_url: string,
-    target_langualge: string
-): Promise<string> {
-    const data = {
-        messages: [
-            { role: 'system', content: "translate all my input to " + target_langualge },
-            { role: 'user', content: message }
-        ],
-        temperature: 0,
-    };
-
+export async function translateWithAzureOpenAI(AZURE_KEY: string, message: string, azure_openai_url: string, targetLanguage: string): Promise<string> {
     try {
-        const parsedUrl = url.parse(azure_openai_url, true);
-        const hostname = parsedUrl.hostname;
-        const pathname = (parsedUrl.pathname || "") + (parsedUrl.search || "");
-
-        const options: https.RequestOptions = {
-            hostname: hostname,
-            path: pathname,
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "api-key": AZURE_KEY,
-            }
+        const data = constructData(message, targetLanguage);
+        const headers = {
+            'Content-Type': 'application/json',
+            'api-key': AZURE_KEY,
         };
 
-        const response: OpenAIResponse = await new Promise((resolve, reject) => {
-            const req = https.request(options, (res: any) => {
-                let body = '';
-                res.setEncoding('utf8');
-                res.on('data', (chunk: any) => {
-                    body += chunk;
-                });
-                res.on('end', () => {
-                    try {
-                        const response = JSON.parse(body) as OpenAIResponse;
-                        resolve(response);
-                    } catch (error: any) {
-                        reject(new Error(`Failed to parse Azure OpenAI response: ${error.message}`));
-                    }
-                });
-            });
-            req.on('error', (error: any) => {
-                reject(new Error(`Request Azure OpenAI failed: ${error.message}`));
-            });
-            req.write(JSON.stringify(data));
-            req.end();
-        });
+        const response = await requestAPI(azure_openai_url, data, headers);
 
         if (response.choices.length === 0) {
             throw new Error('Azure OpenAI no response choices found');
@@ -164,4 +127,3 @@ export async function translateWithAzureOpenAI(
         throw new Error(`Failed to get Azure OpenAI response: ${error.message}`);
     }
 }
-
